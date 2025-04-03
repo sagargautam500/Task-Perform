7e452c73fd420c97bd726cd2e93c7fe6be49ad2a
Sagar Gautam <sagargautam389@gmail.com>
2025-04-03 17:32:24 +0545

    changesCode

--- 1-Rock_paper_Scissor-Game/script.js
+++ 1-Rock_paper_Scissor-Game/script.js
@@ -1,57 +1,48 @@
 let result = document.querySelector(".result");
 let secondPerson = document.querySelector(".secondPerson");
 let firstPerson = document.querySelector(".firstPerson");
- let rock = document.querySelector(".rock");
- let paper = document.querySelector(".paper");
- let scissor = document.querySelector(".scissor");
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissor = document.querySelector(".scissor");
 
 let rule = ["rock", "paper", "scissor"];
 
 let person1, person2;
 function handleRock() {
   person1 = rule[0];
-  console.log(person1)
-  return person1;
+  handleEvent(person1);
 }
 function handlePaper() {
   person1 = rule[1];
-  console.log(person1)
-  return person1;
+  handleEvent(person1);
 }
 function handleScissor() {
   person1 = rule[2];
-  console.log(person1)
-  return person1;
+  handleEvent(person1);
 }
 
+function handleEvent(person1) {
+  person2 = rule[Math.floor(Math.random() * 3)];
+  firstPerson.innerHTML = `User Choose:${person1}`;
+  secondPerson.innerHTML = `Computer Choose :${person2}`;
 
-person1="rock"
-person2 = rule[Math.floor(Math.random() * 3)];
-
+  if (person1 == person2) {
+    result.innerHTML = "game is drawn";
+  } else if (person1 == "rock" && person2 == "scissor") {
+    result.innerHTML = "You wins game";
+  } else if (person1 == "rock" && person2 == "paper") {
+    result.innerHTML = "You lose game";
+  } else if (person1 == "paper" && person2 == "scissor") {
+    result.innerHTML = "you lose game";
+  } else if (person1 == "paper" && person2 == "rock") {
+    result.innerHTML = "you wins game";
+  } else if (person1 == "scissor" && person2 == "rock") {
+    result.innerHTML = "you lose game";
+  } else if (person1 == "scissor" && person2 == "paper") {
+    result.innerHTML = "you wins game";
+  }
+}
 
 rock.addEventListener("click", handleRock);
 paper.addEventListener("click", handlePaper);
 scissor.addEventListener("click", handleScissor);
-
-firstPerson.innerHTML=`User Choose:${person1}`;
-secondPerson.innerHTML= `Computer Choose :${person2}`;
-
-
-if (person1 == person2) {
-  result.innerHTML = "game is drawn";
-} else if (person1 == "rock" && person2 == "scissor") {
-  result.innerHTML = "You wins game";
-} else if (person1 == "rock" && person2 == "paper") {
-  result.innerHTML = "You lose game";
-} else if (person1 == "paper" && person2 == "scissor") {
-  result.innerHTML = "you lose game";
-} else if (person1 == "paper" && person2 == "rock") {
-  result.innerHTML = "you wins game";
-} else if (person1 == "scissor" && person2 == "rock") {
-  result.innerHTML = "you wins game";
-} else if (person1 == "scissor" && person2 == "paper") {
-  result.innerHTML = "you wins game";
-}
-
-// let num=Math.floor(Math.random() * 3);
-// console.log(num)
